7678321d02fe8bfce5e9703289536600a18059c9
Brian Loc Nguyen <NguyenBrianL97@gmail.com>
2018-02-12 01:53:03 -0800

    Added Doom Counter
    Added a folder in Assets "Health Bar" with everything needed for the doom counter.
    
    Added a gameobject in heirachy to show doom counter and attached scripts to it.

---
+++ Assets/Health Bar/Scripts/PlayerISS.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerISS : MonoBehaviour {
+    [SerializeField]
+    private StateISS health;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            health.CurrentVal += 10;
+        }
+    }
+}
