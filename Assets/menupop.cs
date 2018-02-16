fe9221c0f0b733ad7c4b58e2fd75ae5a35d3bdc2
Brian Loc Nguyen <34972651+NguyenBrianL97@users.noreply.github.com>
2018-02-16 11:02:37 -0800

    Laptop
    Latpot

---
+++ Assets/menupop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menupop : MonoBehaviour {
+
+    public GameObject menuUI;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            Runevent();
+        }
+	}
+
+    public void Runevent()
+    {
+        menuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void prdecline()
+    {
+        Debug.Log("PR Level has decreased");
+        /* stat changes */
+    }
+
+    public void praccept()
+    {
+        Debug.Log("PR Level has increase");
+        /*Stat changes*/
+    }
+}
+
+//need to
+//get reference to menugameobject
+//get reference to stat changes
+//not allow any other option; could be done by loading a new scene?
